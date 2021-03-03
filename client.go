68e880c19323cf6696d318d6ae594c96172c2537
yumcyawiz <hashdolf1923@gmail.com>
2021-03-04 05:46:31 +0900

    add comments

--- client.go
+++ client.go
@@ -15,6 +15,7 @@ const (
 )
 
 func main() {
+	// setup a connection to the server
 	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
@@ -22,10 +23,13 @@ func main() {
 	defer conn.Close()
 	c := pb.NewGreeterClient(conn)
 
+	// input user name
 	name := defaultName
 	if len(os.Args) > 1 {
 		name = os.Args[1]
 	}
+
+	// contact the server and print out its response
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 	r, err := c.SayHello(ctx, &pb.Request{Name: name})
--- server.go
+++ server.go
@@ -12,21 +12,25 @@ const (
 	port = ":50051"
 )
 
+// Server is used to implement GreeterServer
 type Server struct {
 	pb.UnimplementedGreeterServer
 }
 
+// implementation of SayHello
 func (s *Server) SayHello(ctx context.Context, in *pb.Request) (*pb.Reply, error) {
 	log.Printf("Received: %v", in.GetName())
 	return &pb.Reply{Message: "Hello " + in.GetName()}, nil
 }
 
 func main() {
+	// listen web server
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 
+	// setup gRPC server
 	s := grpc.NewServer()
 	pb.RegisterGreeterServer(s, &Server{})
 	if err := s.Serve(lis); err != nil {
